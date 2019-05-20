96afc3343c4ca26e81f185f1b4ff6359ef0769b0
mpolski <miroslaw.polski@hpe.com>
2019-05-20 21:44:58 +0200

    reduced number of metrics

--- hpe-oneview-prometheus.go
+++ hpe-oneview-prometheus.go
@@ -18,109 +18,103 @@ func main() {
 	var (
 		clientOV *ov.OVClient
 
-		mEncStatus = prometheus.NewGaugeVec(
+		mStatus = prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
-				Name: "oneview_enclosures_status",
-				Help: "Number of enclosures reported given status.",
+				Name: "oneview_status",
+				Help: "Number of components reported given status.",
 			},
 			[]string{"resourceType", "status"})
 
-		mEncCount = prometheus.NewGaugeVec(
+		mCount = prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
-				Name: "oneview_enclosures_count",
-				Help: "Total number of enclosures.",
+				Name: "oneview_count",
+				Help: "Total number of resources of a given kind.",
 			},
 			[]string{"resourceType"})
 
-		mEncAmbTemp = prometheus.NewGaugeVec(
+		// mEncCount = prometheus.NewGaugeVec(
+		// 	prometheus.GaugeOpts{
+		// 		Name: "oneview_enclosures_count",
+		// 		Help: "Total number of enclosures.",
+		// 	},
+		// 	[]string{"resourceType"})
+
+		mAmbTemp = prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
-				Name: "oneview_enclosures_ambientTemperature_celcius",
-				Help: "Ambient temperature as reported by enclosure.",
+				Name: "oneview_ambientTemperature_celcius",
+				Help: "Ambient temperature as reported by the resource.",
 			},
 			[]string{"category", "uuid", "name"})
-		mEncAvePwr = prometheus.NewGaugeVec(
+		mAvePwr = prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
-				Name: "oneview_enclosures_averagePower_watts",
-				Help: "Average Power consumption as reported by enclosure.",
+				Name: "oneview_averagePower_watts",
+				Help: "Average Power consumption as reported by the resource.",
 			},
 			[]string{"category", "uuid", "name"})
-		mEncPeakPwr = prometheus.NewGaugeVec(
+		mPeakPwr = prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
-				Name: "oneview_enclosures_peakPower_watts",
-				Help: "Peak Power consumption as reported by enclosure.",
+				Name: "oneview_peakPower_watts",
+				Help: "Peak Power consumption as reported by the resource.",
 			},
 			[]string{"category", "uuid", "name"})
 
-		mSrvStatus = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_serverHardware_status",
-				Help: "Number of servers reported given status.",
-			},
-			[]string{"resourceType", "status"})
+		// mSrvCount = prometheus.NewGaugeVec(
+		// 	prometheus.GaugeOpts{
+		// 		Name: "oneview_serverHardware_count",
+		// 		Help: "Total number of server hardware.",
+		// 	},
+		// 	[]string{"resourceType"})
 
-		mSrvCount = prometheus.NewGaugeVec(
+		// mSrvAmbTemp = prometheus.NewGaugeVec(
+		// 	prometheus.GaugeOpts{
+		// 		Name: "oneview_server_ambientTemperature_celcius",
+		// 		Help: "Ambient temperature as reported by server.",
+		// 	},
+		// 	[]string{"category", "uuid", "name"})
+		// mSrvAvePwr = prometheus.NewGaugeVec(
+		// 	prometheus.GaugeOpts{
+		// 		Name: "oneview_server_averagePower_watts",
+		// 		Help: "Average Power consumption as reported by server.",
+		// 	},
+		// 	[]string{"category", "uuid", "name"})
+		mCPUFreq = prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
-				Name: "oneview_serverHardware_count",
-				Help: "Total number of server hardware.",
-			},
-			[]string{"resourceType"})
-
-		mSrvAmbTemp = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_server_ambientTemperature_celcius",
-				Help: "Ambient temperature as reported by server.",
-			},
-			[]string{"category", "uuid", "name"})
-		mSrvAvePwr = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_server_averagePower_watts",
-				Help: "Average Power consumption as reported by server.",
-			},
-			[]string{"category", "uuid", "name"})
-		mSrvAveCPU = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_server_CPUFrequency_Mhz",
-				Help: "Server CPU frequencyd.",
+				Name: "oneview__CPUFrequency_Mhz",
+				Help: "CPU frequency of the resource.",
 			},
 			[]string{"category", "uuid", "name"})
-		mSrvCPUUti = prometheus.NewGaugeVec(
+		mCPUUtil = prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
-				Name: "oneview_server_CPUUtilization_percentage",
-				Help: "Server CPU utilization.",
+				Name: "oneview_CPUUtilization_percent",
+				Help: "CPU utilization of the resource.",
 			},
 			[]string{"category", "uuid", "name"})
-		mSrvPeakPwr = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_server_peakPower_watts",
-				Help: "Peak Power consumption as reported by server.",
-			},
-			[]string{"category", "uuid", "name"})
-		mIntCount = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_interconnect_count",
-				Help: "Total number of Interconnects.",
-			},
-			[]string{"resourceType"})
-		mSasIntCount = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_sasInterconnect_count",
-				Help: "Total number of SAS Interconnects.",
-			},
-			[]string{"resourceType"})
-
-		mSasIntStatus = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "oneview_sasInterconnect_status",
-				Help: "Number of SAS Interconnect modules reporting given status.",
-			},
-			[]string{"resourceType", "status"})
+		// mSrvPeakPwr = prometheus.NewGaugeVec(
+		// 	prometheus.GaugeOpts{
+		// 		Name: "oneview_server_peakPower_watts",
+		// 		Help: "Peak Power consumption as reported by server.",
+		// 	},
+		// 	[]string{"category", "uuid", "name"})
+		// mIntCount = prometheus.NewGaugeVec(
+		// 	prometheus.GaugeOpts{
+		// 		Name: "oneview_interconnect_count",
+		// 		Help: "Total number of Interconnects.",
+		// 	},
+		// 	[]string{"resourceType"})
+		// mSasIntCount = prometheus.NewGaugeVec(
+		// 	prometheus.GaugeOpts{
+		// 		Name: "oneview_sasInterconnect_count",
+		// 		Help: "Total number of SAS Interconnects.",
+		// 	},
+		// 	[]string{"resourceType"})
 	)
 
 	prometheus.MustRegister(
-		mEncCount, mEncStatus, mEncAmbTemp, mEncAvePwr, mEncPeakPwr,
-		mSrvCount, mSrvStatus, mSrvAmbTemp, mSrvAvePwr, mSrvAveCPU, mSrvCPUUti, mSrvPeakPwr,
-		mIntCount, //mIntStatus,
-		mSasIntCount, mSasIntStatus,
+		mStatus, mCount, mAmbTemp, mAvePwr, mPeakPwr, mCPUFreq, mCPUUtil,
+		//mEncAmbTemp, mEncAvePwr, mEncPeakPwr, //mEncStatus, ,mEncCount
+		//mSrvAmbTemp, mSrvAvePwr, mSrvAveCPU, mSrvCPUUti, mSrvPeakPwr, //mSrvStatus, mSrvCount
+		//mIntCount,    //mIntStatus,
+		//mSasIntCount, // mSasIntStatus,
 	)
 
 	//Enclosure Count, ServerHardware Count, Interconnect Count, SAS Interconnect Count
@@ -140,7 +134,7 @@ func main() {
 				fmt.Println("Enclosures Retrieval Failed: ", err)
 			} else {
 				fmt.Println("#----------------Enclosures Count Total---------------#", encC.Total)
-				mEncCount.With(prometheus.Labels{"resourceType": encC.Category}).Set(float64(encC.Total))
+				mCount.With(prometheus.Labels{"resourceType": encC.Category}).Set(float64(encC.Total))
 			}
 
 			//ServerHardware count
@@ -149,7 +143,7 @@ func main() {
 				fmt.Println("Server Hardware Retrieval Failed: ", err)
 			} else {
 				fmt.Println("#----------------Server Hardware Count Total---------------#", srvC.Total)
-				mSrvCount.With(prometheus.Labels{"resourceType": srvC.Category}).Set(float64(srvC.Total))
+				mCount.With(prometheus.Labels{"resourceType": srvC.Category}).Set(float64(srvC.Total))
 			}
 
 			//Interconnect count
@@ -158,7 +152,7 @@ func main() {
 				fmt.Println("Interconnect Retrieval Failed: ", err)
 			} else {
 				fmt.Println("#----------------Interconnect Count Total---------------#", intC.Total)
-				mIntCount.With(prometheus.Labels{"resourceType": intC.Category}).Set(float64(intC.Total))
+				mCount.With(prometheus.Labels{"resourceType": intC.Category}).Set(float64(intC.Total))
 			}
 
 			//SAS Interconnect count
@@ -167,7 +161,7 @@ func main() {
 				fmt.Println("SAS Interconnect Retrieval Failed: ", err)
 			} else {
 				fmt.Println("#----------------SAS Interconnect Count Total---------------#", sintC.Total)
-				mSasIntCount.With(prometheus.Labels{"resourceType": sintC.Category}).Set(float64(sintC.Total))
+				mCount.With(prometheus.Labels{"resourceType": sintC.Category}).Set(float64(sintC.Total))
 			}
 
 			fmt.Println(time.Now().Format(time.RFC3339))
@@ -199,13 +193,13 @@ func main() {
 
 			for _, s := range f {
 
-				//Enclsolure status
+				//Enclosure status
 				encS, err := ovc.GetEnclosures("", "", s[0], "", "")
 				if err != nil {
 					fmt.Println("Enclosure Retrieval Failed: ", err)
 				} else {
 					fmt.Println("#----------------Enclosure Count with", s[0], "---------------#", encS.Total)
-					mEncStatus.With(prometheus.Labels{"resourceType": encS.Category, "status": s[0]}).Set(float64(encS.Total))
+					mStatus.With(prometheus.Labels{"resourceType": encS.Category, "status": s[0]}).Set(float64(encS.Total))
 				}
 				//ServerHardware status
 				srvS, err := ovc.GetServerHardwareList(s, "")
@@ -213,7 +207,7 @@ func main() {
 					fmt.Println("Server Hardware Retrieval Failed: ", err)
 				} else {
 					fmt.Println("#----------------Server Hardware Count with", s[0], "---------------#", srvS.Total)
-					mSrvStatus.With(prometheus.Labels{"resourceType": srvS.Category, "status": s[0]}).Set(float64(srvS.Total))
+					mStatus.With(prometheus.Labels{"resourceType": srvS.Category, "status": s[0]}).Set(float64(srvS.Total))
 				}
 				//Interconnects status - not currently supported - needs a workaround
 				// intS, err := ovc.GetInterconnects("", "", "'OK'", "")
@@ -230,7 +224,7 @@ func main() {
 					fmt.Println("SAS Interconnect Retrieval Failed: ", err)
 				} else {
 					fmt.Println("#----------------SAS Interconnect Count with", s[0], "---------------#", sintS.Total)
-					mSasIntStatus.With(prometheus.Labels{"resourceType": sintS.Category, "status": s[0]}).Set(float64(sintS.Total))
+					mStatus.With(prometheus.Labels{"resourceType": sintS.Category, "status": s[0]}).Set(float64(sintS.Total))
 				}
 			}
 
@@ -268,14 +262,14 @@ func main() {
 					//if data not fresh, set values to 0
 					if w.IsFresh == false {
 						fmt.Println("No fresh utilization data vailable for -", name, "-...skipping")
-						mEncAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
-						mEncAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
-						mEncPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+						mAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+						mAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+						mPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
 					} else {
 						for j := 0; j < len(w.MetricList); j++ {
-							mEncAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(w.MetricList[0].MetricSamples[0][1].(float64))
-							mEncAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(w.MetricList[1].MetricSamples[0][1].(float64))
-							mEncPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(w.MetricList[2].MetricSamples[0][1].(float64))
+							mAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(w.MetricList[0].MetricSamples[0][1].(float64))
+							mAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(w.MetricList[1].MetricSamples[0][1].(float64))
+							mPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(w.MetricList[2].MetricSamples[0][1].(float64))
 
 							fmt.Println("Category:", category, "UUID: ", UUID, ", Name: ", name, ", Metric:", w.MetricList[j].MetricName, ", Value: ", w.MetricList[j].MetricSamples[0][1], ", Sample Time: ", w.MetricList[j].MetricSamples[0][0])
 						}
@@ -316,18 +310,18 @@ func main() {
 				//if data not fresh, set values to 0
 				if u.IsFresh == false {
 					fmt.Println("No fresh utilization data available for -", name, "-...skipping")
-					mSrvAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
-					mSrvAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
-					mSrvAveCPU.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
-					mSrvCPUUti.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
-					mSrvPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+					mAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+					mAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+					mCPUFreq.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+					mCPUUtil.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
+					mPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(0)
 				} else {
 					for j := 0; j < len(u.MetricList); j++ {
-						mSrvAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[0].MetricSamples[0][1].(float64))
-						mSrvAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[1].MetricSamples[0][1].(float64))
-						mSrvAveCPU.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[2].MetricSamples[0][1].(float64))
-						mSrvCPUUti.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[3].MetricSamples[0][1].(float64))
-						mSrvPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[4].MetricSamples[0][1].(float64))
+						mAmbTemp.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[0].MetricSamples[0][1].(float64))
+						mAvePwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[1].MetricSamples[0][1].(float64))
+						mCPUFreq.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[2].MetricSamples[0][1].(float64))
+						mCPUUtil.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[3].MetricSamples[0][1].(float64))
+						mPeakPwr.With(prometheus.Labels{"category": category, "uuid": UUID, "name": name}).Set(u.MetricList[4].MetricSamples[0][1].(float64))
 
 						fmt.Println("Category:", category, "UUID: ", UUID, ", Name: ", name, ", Metric:", u.MetricList[j].MetricName, ", Value: ", u.MetricList[j].MetricSamples[0][1], ", Sample Time: ", u.MetricList[j].MetricSamples[0][0])
 					}
